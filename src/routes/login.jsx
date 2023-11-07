5e2509f50c73bb5b3828fb413be301c27c0c8c79
gganster <guilian.ganster@wisoftify.fr>
2023-11-07 03:06:45 +0100

    form de login

--- src/routes/login.jsx
+++ src/routes/login.jsx
@@ -1,25 +1,67 @@
-import { Card, CardHeader, CardTitle, CardContent, CardFooter } from "@/components/ui/card"
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod"
+import * as z from "zod";
+
+import { Card, CardHeader, CardTitle, CardContent, CardFooter } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
-import { Label } from "@radix-ui/react-label";
 import { Button } from "@/components/ui/button";
+import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
+import { useCallback } from "react";
+
+const formScheme = z.object({
+  email: z.string().email({ message: "Please enter a valid email"}),
+  password: z.string().min(8, { message: "Password must be at least 8 characters long"}),
+});
 
 const Login = () => {
+  const form = useForm({
+    resolver: zodResolver(formScheme),
+    defaultValues: {
+      email: "",
+      password: "",
+    }
+  });
+
+  const onSubmit = useCallback((data) => {
+    console.log(data);
+  }, []);
+
   return (
-    <div className="w-screen flex items-center justify-center bg-red-600" >
-      <Card>
-        <CardHeader>
-          <CardTitle>Login</CardTitle>
-        </CardHeader>
-        <CardContent>
-          <Label>Email:</Label>
-          <Input />
-          <Label>Password:</Label>
-          <Input />
-        </CardContent>
-        <CardFooter>
-          <Button variant="primary">Login</Button>
-        </CardFooter>
-      </Card>
+    <div className="h-screen w-screen flex items-center justify-center" >
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)}>
+          <Card>
+            <CardHeader>
+              <CardTitle>Login</CardTitle>
+            </CardHeader>
+            <CardContent>
+              <FormField control={form.control} name="email" render={({field}) => (
+                <FormItem>
+                  <FormLabel htmlFor="email">Email</FormLabel>
+                  <FormControl>
+                    <Input type="email" id="email" placeholder="@" {...field} />
+                  </FormControl>
+                  <FormDescription />
+                  <FormMessage />
+                </FormItem>
+              )} />
+              <FormField control={form.control} name="password" render={({field}) => (
+                <FormItem>
+                  <FormLabel htmlFor="password">Password</FormLabel>
+                  <FormControl>
+                    <Input type="password" placeholder="******" id="password" {...field} />
+                  </FormControl>
+                  <FormDescription />
+                  <FormMessage />
+                </FormItem>
+              )} />
+            </CardContent>
+            <CardFooter className="justify-center">
+              <Button type="submit">Login</Button>
+            </CardFooter>
+          </Card>
+        </form>
+      </Form>
     </div>
   )
 }
